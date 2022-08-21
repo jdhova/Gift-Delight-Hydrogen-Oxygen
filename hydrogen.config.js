f98716c94f7f601b1af17950ddad5a8c17398211
Jude Okagu <juud0010@gmail.com>
2022-08-21 11:20:54 -0400

    linked to shop front

---
+++ hydrogen.config.js
@@ -0,0 +1,20 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'guifs-delight.myshopify.com',
+    // @ts-ignore
+    storefrontToken: 'ed5d8cf35f92b74e1d55d39d9aeff876',
+    // @ts-ignore
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
